ebdef02395de889cb4b931f3e4e384e9ba620b64
Ekaterina Pavluk <rinapalijlinnik@gmail.com>
2023-09-28 13:56:36 +0300

    add delete and update def

--- phonebook_old.py
+++ phonebook_old.py
@@ -25,10 +25,11 @@ def main():
         while True:
 
             print("1. Ввести данные")    
-            print("2. Поиск")    
-            print("3. Выход")
+            print("2. Поиск") 
+            print("3. Изменить/Удалить данные")
+            print("4. Выход")
             user_choice = input("Введите:")
-            if user_choice not in ["1","2","3"]:
+            if user_choice not in ["1","2","3","4"]:
                 print("Ошибка")
             else:
                 break
@@ -38,6 +39,8 @@ def main():
             case "2":
                 search_data(data)
             case "3":
+                update_or_delete_data(data)    
+            case "4":
                 print("Выход")
                 if not data:
                     return
@@ -58,16 +61,17 @@ def input_data(data):
     print("Неверный ввод данных")   
     return data                     
 
+
 def search_data(data):
     user_input = input("Введите данные для поиска: ")
     while True:
     
-            print("1. Найти фамилию")    
-            print("2. Найти имя")    
-            print("3. Найти отчество")
-            print("4. Найти номер телефона")
-            print("5. Вернуться в меню")
-            user_choice = ("Введите:")
+            print("1. Найти фамилию:")    
+            print("2. Найти имя:")    
+            print("3. Найти отчество:")
+            print("4. Найти номер телефона:")
+            print("5. Вернуться в меню:")
+            user_choice = input("Введите:")
             if user_choice not in ["1","2","3","4","5"]:
                 print("Ошибка")
             else:
@@ -76,19 +80,19 @@ def search_data(data):
     match user_choice: 
             case "1":
                 for key in data:
-                    name1, name2, name3 = key.split()
+                    name1, name2, name3 = key.clear()
                     if name1 == user_input:
                         print(f"{key} {data[key]}")
             
             case "2":
                 for key in data:
-                    name1, name2, name3 = key.split()
+                    name1, name2, name3 = key.clear()
                     if name2 == user_input:
                         print(f"{key} {data[key]}")
             
             case "3":
                 for key in data:
-                    name1, name2, name3 = key.split()
+                    name1, name2, name3 = key.clear()
                     if name3 == user_input:
                         print(f"{key} {data[key]}")
                 
@@ -100,11 +104,60 @@ def search_data(data):
             case "5":
                 print("Выход")
                 
-                return   
+                return  
+
+
+def update_or_delete_data(data):
+    while True:
+    
+            print("1. Изменить данные:")    
+            print("2. Удалить данные:")    
+            print("3. Вернуться в меню:")
+            user_choice = input("Введите:")
+            if user_choice not in ["1","2","3"]:
+                print("Ошибка")
+            else:
+                break
+
+    if user_choice == "1":
+        update_data(data)  
+    elif user_choice == "2":
+        delete_data(data)
+    elif user_choice == "3":
+        print("Выход")
+
+
+def update_data(data):
+    user_input = input("Введите ФИО для изменения данных: ")        
+    if user_input in data:
+        new_num = input("Введите новый номер телефона: ")
+        if new_num.isdigit():
+            data[user_input] = new_num
+            print("Данные обновлены: ") 
+        else:
+            print("Ошибка.Номер не найден")
+    else:
+        print("Ошибка.Данные не найдены")
+
+
+def delete_data(data):
+    user_input = input("Введите ФИО для удаления данных: ")        
+    if user_input in data:
+        del data[user_input]
+        print("Данные удалены")
+    else:
+        print("Данные не найдены")
+
+
+def save_data(data):
+    with open(NAME_FILE, "w") as f:
+                    for name in data:
+                        print(f"{name}\t{data[name]}", file=f)
+                
 
 if __name__   == "__main__" :
     main()                      
 
 
-# Задача 38: Дополнить телефонный справочник возможностью изменения и удаления данных. Пользователь также может ввести имя или фамилию,
-# и Вы должны реализовать функционал для изменения и удаления данных.
+# Задача 38: Дополнить телефонный справочник возможностью изменения и удаления данных. Пользователь также может 
+# ввести имя или фамилию,и Вы должны реализовать функционал для изменения и удаления данных.
